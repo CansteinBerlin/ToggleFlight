8792a86db266a259ca5da69db88e779f7cbbe2e9
Hasenzahn1 <motzer10@gmx.de>
2024-02-29 15:39:28 +0100

    Update Toggleflight command for no argument usage.

--- src/main/java/me/hasenzahn1/toggleflight/ToggleFlightCommand.java
+++ src/main/java/me/hasenzahn1/toggleflight/ToggleFlightCommand.java
@@ -10,31 +10,36 @@ public class ToggleFlightCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!sender.hasPermission("toggleflight.command.toggleflight")){
-            sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("noPermission"));
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("hasToBePlayer"));
             return true;
         }
-
-        if(args.length != 1){
-            sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("invalidCommand", "command", command.getName()));
-            return true;
+        Player applicator = ((Player) sender);
+        if (args.length == 0) {
+            if (!sender.hasPermission("toggleflight.command.toggleflight.self")) {
+                sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("noPermission"));
+                return true;
+            }
+        } else {
+            if (!sender.hasPermission("toggleflight.command.toggleflight.other")) {
+                sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("noPermission"));
+                return true;
+            }
+            applicator = Bukkit.getPlayer(args[0]);
+            if (applicator == null) {
+                sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("noPlayer", "player", args[0]));
+                return true;
+            }
         }
 
-        Player p = Bukkit.getPlayer(args[0]);
-        if(p == null){
-            sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("noPlayer", "player", args[0]));
-            return true;
+        if (ToggleFlight.INSTANCE.getEnabledPlayers().contains(applicator.getUniqueId())) {
+            ToggleFlight.INSTANCE.getEnabledPlayers().remove(applicator.getUniqueId());
+        } else {
+            ToggleFlight.INSTANCE.getEnabledPlayers().add(applicator.getUniqueId());
         }
 
-
-        if(ToggleFlight.INSTANCE.getEnabledPlayers().contains(p.getUniqueId())){
-            ToggleFlight.INSTANCE.getEnabledPlayers().remove(p.getUniqueId());
-        }else{
-            ToggleFlight.INSTANCE.getEnabledPlayers().add(p.getUniqueId());
-        }
-
-        sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("success", "player", p.getDisplayName(), "state",
-                ToggleFlight.getLang(ToggleFlight.INSTANCE.getEnabledPlayers().contains(p.getUniqueId()) ? "enabled" : "disabled")));
+        sender.sendMessage(ToggleFlight.PREFIX + ToggleFlight.getLang("success", "player", applicator.getDisplayName(), "state",
+                ToggleFlight.getLang(ToggleFlight.INSTANCE.getEnabledPlayers().contains(applicator.getUniqueId()) ? "enabled" : "disabled")));
 
         return true;
     }
